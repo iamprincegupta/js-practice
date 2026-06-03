be9b22315fa318146d03a7ce098504dd496398ee
Prince <prince90165@gmail.com>
2026-06-03 11:53:53 +0530

    Day 3: fixed FizzBuzz condition order

--- day3.js
+++ day3.js
@@ -41,12 +41,12 @@ for (let i=1; i<=10; i++) {
 
 
 for (let i=1; i<=30; i++){
-    if(i % 3 === 0){
+    if(i % 15 === 0){
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0){
         console.log("Fizz")
     } else if (i % 5 === 0){
         console.log("Buzz")
-    } else if (i % 15 === 0){
-        console.log("FizzBuzz")
     } else {
         console.log(i)
     }
